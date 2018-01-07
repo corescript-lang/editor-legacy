329fcb9da0da6ef6aea0b634315f2228b2861124
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-06 23:43:33 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,15 @@
 var vars = [];
+var cc;
+var params = [];
+var functionname;
+var datadata;
 function run(code) {
-var c = code.split("/n");
+  var c = code.split("/n");
+  for(var i=0;i<c.length;i++) {
+    cc = c[i].split(" ");
+    functionname = cc[0];
+    cc.shift();
+    datadata = cc.join("");
+    params = datadata.split(",");
+  }
 }
