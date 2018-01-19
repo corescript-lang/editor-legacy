62a2a9cec3c2bf558dde68d7ef80bd1950bf084d
Pufflegamerz <32369330+Pufflegamerz@users.noreply.github.com>
2018-01-19 12:01:09 -0500

    Add arrays

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+var syntax = true;
 var variables = [];
 var labels = [];
 var commands = [];
@@ -22,6 +23,7 @@ function exec() {
             current = current.split(":")[1];
         }
         var valid = false;
+        current = current.replace(/\/\/[^]+/g,"");
         if (current.startsWith("var ")) {
             if (current.includes("=")) {
                 var var1 = current.substring(4);
@@ -36,11 +38,13 @@ function exec() {
                     if (var3.startsWith("random(") && endsWith(")")) {
                         var random1 = var3.substring(7);
                     }
-                    variables.push(var2 + "=" + var3);
+                    variables.push(var2 + "=" + replaceVars(var3));
                 }
             } else {
                 alert("Syntax Error on line " + (i + 1) + ".");
             }
+        } else if (current == "stop") {
+            return
         } else if (current.startsWith("print ")) {
             var print1 = current.substring(6);
             if (repeat) {
@@ -79,7 +83,7 @@ function exec() {
                 i = +goto1 - 2;
             }
         } else if (current.startsWith(":")) {
-            i++
+
         } else if (current.startsWith("msg ")) {
             var msg1 = current.substring(4);
             if (repeat) {
@@ -147,6 +151,8 @@ function exec() {
             } else {
                 alert("Syntax Error on line " + (i + 1) + ".");
             }
+        } else if (current.startsWith("set ")) {
+
         } else if (current.endsWith("++")) {
             var plusplus = current.substring(0, current.length - 2);
             if (repeat) {
@@ -171,8 +177,9 @@ function exec() {
 
         } else if (current == "") {
 
-        } else if (current == "stop") {
-            return
+        } else if (current == "Meeska! Mooska! Mickey Mouse!") {
+            // Easter Eggs!
+            document.getElementById('player').innerHTML += "<img width='400' src='https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/abc-MMC_VOLUME_010-Full-Image_GalleryBackground-en-US-1504651650211._RI_SX940_.jpg'>";
         } else {
             for (var o = 0; o < commands.length; o++) {
                 if (current.startsWith(commands[o].split(",")[0])) {
@@ -226,19 +233,27 @@ function replaceVars(string) {
         if (add == null && random == null) {
             for (var w = 0; w < variables.length; w++) {
                 var name = variables[w].split("=")[0];
-                var value = variables[w].split("=")[1]; 
-                result = result.replace("(" + name + ")", value);
+                var value = variables[w].split("=")[1];
+                if (value.startsWith("[") && value.endsWith("]")) {
+                    var array = value.substring(1,value.length - 1).split(",");
+                    for (var z = 0; z < 30; z++) {
+                        result = result.replace("(" + name + "[" + z + "])", array[z - 1]);
+                    }
+                } else {
+                    result = result.replace("(" + name + ")", value);
+                }
+                
             }
         } else {
             if (random == null) {
                 result = result.replace(add[0],(+add[0].substring(5,add[0].length-1).split(",")[0]) + (+add[0].substring(5,add[0].length-1).split(",")[1]));
             } else {
-                result = result.replace(random[0], Math.floor((Math.random() * (+random[0].substring(7,random[0].length-1).split(",")[1])) + (+random[0].substring(7,random[0].length-1).split(",")[0])));
+                result = result.replace(random[0], Math.floor((Math.random() * (+random[0].substring(7,random[0].length-1).split(",")[1])) + (+random[0].substring(7,random[0].length-1).split(",")[0]))).toString();
             }
            
         }
     }
-    return result.toString();
+    return result
 
 
 }
@@ -252,37 +267,64 @@ function download() {
     element.click();
     document.body.removeChild(element);
 }
+function upload() {
+    var upload = document.createElement('INPUT');
+    upload.type = "file";
+    document.body.appendChild(upload);
+    upload.click();
+     var file = upload.files[0];
+  if (file) {
+      var reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = function(evt) {
+         alert(evt.target.result);
+      }
+      reader.onerror = function(evt) {
+          alert("Error");
+      }
+  }
+    document.body.removeChild(upload);
+}
 function update() {
-    var text = document.getElementById('code').value;
-    var regex4 = /var ([^\n=]+)([//=]+)([^\n=]+)/g;
-    var regex1 = /\/\/([a-zA-Z0-9~!@#$%^&* =;'`]*)/g;
-    var regex3 = /print ([^\n]+)/g;
-    var regex12 = /msg ([^\n]+)/g;
-    var regex5 = /([^\n]+)\+\+/g;
-    var regex6 = /([^\n]+)--/g;
-    var regex8 = /\(([^)(]+)\)/g;
-    var regex7 = /input ([^\n=]+)([//=]+)([^\n=]+)/g;
-    var regex9 = /if ([^\n=]+)([//=]+)([^\n=]+)/g;
-    var regex10 = /not ([^\n=]+)([//=]+)([^\n=]+)/g;
-    var regex11 = /\n:([^\n=]+)/g;
-    var regex13 = /repeat ([0-9]+):/g;
-    text = text.replace(/</g,"<&zwnj;"); 
-    text = text.replace(/>/g,"&zwnj;>");
-    // text = text.replace(regex8, "(<span style='color:white;'>$1</span>)");
-    text = text.replace(regex11, "<br><span style='color:lightblue;'>:$1</span>");
-    text = text.replace(regex4, "<span style='color:rgb(0,128,155);'>var</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
-    text = text.replace(regex3, "<span style='color:gold;'>print </span><span style='color:rgb(230,219,116);'>$1</span>");
-    text = text.replace(regex9, "<span style='color:rgb(0,128,155);'>if</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
-    text = text.replace(regex1, "<span style='color:rgb(150,150,150);'>//$1</span>");
-    text = text.replace(regex10, "<span style='color:rgb(0,128,155);'>not</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
-    text = text.replace(/stop/g, "<span style='color:red;'>stop</span>");
-    text = text.replace(regex5, "<span style='color:lightgreen;'>$1++</span>");
-    text = text.replace(regex6, "<span style='color:red;'>$1--</span>");
-    text = text.replace(regex12, "<span style='color:cyan;'>msg </span><span style='color:rgb(230,219,116);'>$1</span>");
-    text = text.replace(regex7, "<span style='color:rgb(0,128,155);'>input</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
-    text = text.replace(regex13,"<span style='color:lightgreen;'>repeat $1:</span>");
-    text = text.toString().replace(/\n/g, "<br>");
-    document.getElementById("overlay").innerHTML = text;
+    if (syntax) {
+        document.getElementById("overlay").style.visibility = "visible";
+        document.getElementById("code").style.color = "transparent";
+        document.getElementById("code").style.backgroundColor = "transparent";
+        var text = document.getElementById('code').value;
+        var regex4 = /var ([^\n=]+)([//=]+)([^\n=]+)/g;
+        var regex1 = /\/\/([^]*)/g;
+        var regex3 = /print ([^\n]+)/g;
+        var regex12 = /msg ([^\n]+)/g;
+        var regex5 = /([^\n]+)\+\+/g;
+        var regex6 = /([^\n]+)--/g;
+        var regex8 = /\(([^)(]+)\)/g;
+        var regex7 = /input ([^\n=]+)([//=]+)([^\n=]+)/g;
+        var regex9 = /if ([^\n=]+)([//=]+)([^\n=]+)/g;
+        var regex10 = /not ([^\n=]+)([//=]+)([^\n=]+)/g;
+        var regex11 = /\n:([^\n=]+)/g;
+        var regex13 = /repeat ([0-9]+):/g;
+        text = text.replace(/</g,"<&zwnj;"); 
+        text = text.replace(/>/g,"&zwnj;>");
+        // text = text.replace(regex8, "(<span style='color:white;'>$1</span>)");
+        text = text.replace(regex11, "<br><span style='color:lightblue;'>:$1</span>");
+        text = text.replace(regex4, "<span style='color:rgb(0,128,155);'>var</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
+        text = text.replace(regex3, "<span style='color:gold;'>print </span><span style='color:rgb(230,219,116);'>$1</span>");
+        text = text.replace(regex9, "<span><span style='color:rgb(0,128,155);'>if</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span></span><span></span>");
+        text = text.replace(regex1, "<span style='color:rgb(150,150,150);'>//$1</span>");
+        text = text.replace(regex10, "<span style='color:rgb(0,128,155);'>not</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
+        text = text.replace(/stop/g, "<span style='color:red;'>stop</span>");
+        text = text.replace(regex5, "<span style='color:lightgreen;'>$1++</span>");
+        text = text.replace(regex6, "<span style='color:red;'>$1--</span>");
+        text = text.replace(regex12, "<span style='color:cyan;'>msg </span><span style='color:rgb(230,219,116);'>$1</span>");
+        text = text.replace(regex7, "<span style='color:rgb(0,128,155);'>input</span> <span style='color:rgb(230,219,116);'>$1</span><span style='color:crimson;'>$2</span><span style='color:rgb(230,219,116);'>$3</span>");
+        text = text.replace(regex13,"<span style='color:lightgreen;'>repeat $1:</span>");
+        text = text.toString().replace(/\n/g, "<br>");
+        document.getElementById("overlay").innerHTML = text;
+    } else {
+        document.getElementById("overlay").style.visibility = "hidden";
+        document.getElementById("code").style.color = "white";
+        document.getElementById("code").style.backgroundColor = "rgb(66,66,66)";
+    }
     count();
 }
 // window.onbeforeunload = function(e) {
